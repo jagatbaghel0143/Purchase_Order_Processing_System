f1375f2f054f2ac713af79a5c09dc4e3c9f97689
Jagat Bhan Singh <jagatbaghel8979@gmail.com>
2023-05-22 20:10:19 +0530

    Implemented Delete Plant functionality

--- Administration.cpp
+++ Administration.cpp
@@ -12,18 +12,19 @@ ReadAndWrite readAndWrite;
 
 // Administration class for managing  admin activities
 void Administration::addPlant() {
+    system("clear");
     int id = time(nullptr);
     string name;
     string loc;
     int laborers;
     int capacity;
-    cout<<"\n"<<"Enter Plant Name: ";
+    cout<<"\n\t\tEnter Plant Name: ";
     cin>>name;
-    cout<<"Enter Plant Location: ";
+    cout<<"\t\tEnter Plant Location: ";
     cin>>loc;
-    cout<<"Enter Loborer Count: ";
+    cout<<"\t\tEnter Loborer Count: ";
     cin>>laborers;
-    cout<<"Enter Capacity Per Month: ";
+    cout<<"\t\tEnter Capacity Per Month: ";
     cin>>capacity;
     Plant plant(id, name, loc, laborers, capacity);
     string data = readAndWrite.readDataFromFile();
@@ -32,17 +33,19 @@ void Administration::addPlant() {
     data = conversionUtility.convertVectorToString(plants);
     if(readAndWrite.writeDataToFile(data)) {
         int idd = plant.getPlantId();
-        cout<<"\nNew manufacturing unit with ID #"<<idd<<" is added successfully!\n";
+        cout<<"\n\tNew manufacturing unit with ID #"<<idd<<" is added successfully!";
     } else {
-        cout<<"\nError while adding new plant";
+        cout<<"\n\t\tError while adding new plant";
     }
+    cout<<"\n\t\tPress ENTER to continue...";
+    cin.ignore();
     cin.get();
 }
 
 void Administration::updatePlant() {
     system("clear");
     int plantId;
-    cout<<"Enter Plant ID which you want to update: ";
+    cout<<"\t\tEnter Plant ID which you want to update: ";
     cin>>plantId;
     bool flag = true, updateStatus = false;
     string data = readAndWrite.readDataFromFile();
@@ -109,5 +112,60 @@ void Administration::updatePlant() {
         cout << "\n\t\tOpps! No plant exists with this #"<<plantId<<" Id\n\n";
     }
     cout<<"Press ENTER to continue...";
-    getchar();
+    cin.ignore();
+    cin.get();
+}
+
+void Administration::deletePlant() {
+    system("clear");
+    int plantId;
+    cout<<"Enter Plant ID which you want to delete: ";
+    cin>>plantId;
+    bool flag = true, updateStatus = false;
+    string data = readAndWrite.readDataFromFile();
+    vector<Plant> plant = conversionUtility.convertStringToVector(data);
+    for(auto i = plant.begin(); i != plant.end(); ++i) {
+        if(i->getPlantId() == plantId) {
+            int choice = -1;
+            flag = false;
+            cout<<"\t\tPlant Details are as follows:";
+            cout<<"\n\t\tPlant Id: "<<i->getPlantId();
+            cout<<"\n\t\tPlant Name: "<<i->getPlantName();
+            cout<<"\n\t\tPlant Location: "<<i->getPlantLoc();
+            cout<<"\n\t\tLaborer Count: "<<i->getLaborerCount();
+            cout<<"\n\t\tCapacity Per Month: "<<i->getCapacityPerMonth();
+            cout<<"\n\n\t\tAre you sure you want to delete it...";
+            cout<<"\n\t\t1. YES";
+            cout<<"\n\t\t2. NO";
+            cout<<"\nEnter your choice to update [1,2]: ";
+            cin>>choice;
+
+            switch(choice) {
+                case 1:
+                    plant.erase(i);
+                    break;
+                default:
+                    break;
+            }
+            if(choice == 2) {
+                break;
+            } else if ((choice > 0)  && (choice < 2)) {
+                updateStatus = true;
+            }
+            if(updateStatus) {
+                if(readAndWrite.writeDataToFile(conversionUtility.convertVectorToString(plant))) {
+                    cout<<"\n\t\tPlant with id #"<<plantId<<" deleted successfully!\n\n";
+                }else {
+                cout<<"\nError while deleting plant details...";
+                }
+            }
+            break;
+        }
+    }
+    if(flag) {
+        cout << "\n\t\tOpps! No plant exists with this #"<<plantId<<" Id\n\n";
+    }
+    cout<<"Press ENTER to continue...";
+    cin.ignore();
+    cin.get();
 }
