1bea5d0e1ef383ab88b07ceeaec393ed4fb0df60
Jagat Bhan Singh <jagatbaghel8979@gmail.com>
2023-05-22 20:13:30 +0530

    Refactored code

--- User.cpp
+++ User.cpp
@@ -12,14 +12,14 @@ void User::setLogins(string s1, string s2) {
 
 void User::login() {
     system("clear");
-    cout<<"\n \t\t\t\tEnter your username and password to continue!";
-    cout<<"\n \t\t\t\t USERNAME: ";
+    cout<<"\n \t\tEnter your username and password to continue!";
+    cout<<"\n \t\t USERNAME: ";
     cin>>username;
-    cout<<" \n \t\t\t\t PASSWORD: ";
+    cout<<"\t\t PASSWORD: ";
     cin>>password;
     if(u == username && p == password) {
-        cout<<"\n\n"<<"\t Congratulations!! You have successfully logged into the System!\n";
-        cout<<"\n \t\t Press ENTER to continue...";
+        cout<<"\n\t Congratulations!! You have successfully logged into the System!";
+        cout<<"\n\t\t Press ENTER to continue...";
         cin.ignore();
         cin.get();
 
