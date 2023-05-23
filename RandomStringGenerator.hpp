9cf506314583bc20c933b7692725d880d3167706
Jagat Bhan Singh <jagatbaghel8979@gmail.com>
2023-05-23 21:52:35 +0530

    Added Random String Generator to make a header file

---
+++ RandomStringGenerator.hpp
@@ -0,0 +1,11 @@
+#ifndef RANDOMSTRINGGENERATOR_HPP
+#define RANDOMSTRINGGENERATOR_HPP
+#include <string>
+using namespace std;
+
+class RandomStringGenerator {
+    public:
+        string generateRandomString(int length);
+};
+
+#endif
